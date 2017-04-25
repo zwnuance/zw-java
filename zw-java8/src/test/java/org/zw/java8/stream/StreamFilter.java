3db6900955488b2dc3e907d69dc426a2f56b1685
zwnuance <zwnuance@gmail.com>
2017-04-25 15:55:02 +0800

    mend

--- zw-java8/src/test/java/org/zw/java8/stream/StreamFilter.java
+++ zw-java8/src/test/java/org/zw/java8/stream/StreamFilter.java
@@ -1,13 +1,17 @@
 package org.zw.java8.stream;
 
 import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,7 +27,7 @@ public class StreamFilter {
 
   @Before
   public void init() throws IOException {
-    String contents = new String(Files.readAllBytes(Paths.get("D:\\TMP\\alice.txt")), Charsets.UTF_8);
+    String contents = new String(Files.readAllBytes(Paths.get(getClass().getResource("/alice.txt").getFile().substring(1))), Charsets.UTF_8);
     words = Arrays.asList(contents.split("[\\P{L}]"));
   }
 
@@ -52,7 +56,7 @@ public class StreamFilter {
 
   @Test
   public void testSubStream() throws IOException {
-    Stream<String> lines = Files.lines(Paths.get("D:\\TMP\\alice.txt"));
+    Stream<String> lines = Files.lines(Paths.get(getClass().getResource("/alice.txt").getFile().substring(1)));
     long count =
         lines.map(w -> Arrays.asList(w.split("[\\P{L}]")).stream().filter(c -> c.length() > 12).count())
             .reduce((sum, c) -> sum + c).get();
